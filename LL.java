cbeab8306f84c22a49696181c86e7e04169ca21e
Winterflakes <sakshirs1234@gmail.com>
2024-05-01 15:24:13 +0530

    day 1 dsa in java linked list

--- LL.java
+++ LL.java
@@ -11,12 +11,14 @@ public class LL{
     }
     
     public static Node head;
-    public static Node tail;
+    public static int size;
 
     public void addFirst(int data){
+        Node temp=head;
         Node newNode = new Node(data);
-        if(head==null){
-            head = tail = newNode;
+        size++;
+        if(temp==null){
+            head=newNode;
             return;
         }
         newNode.next = head;
@@ -24,20 +26,52 @@ public class LL{
     }
 
     public void addLast(int data){
+        Node temp=head;
         Node newNode = new Node(data);
-        if(head==null){
-            head = tail = newNode;
+        size++;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public void print(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + "-->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void add(int index,int data){
+        if(index == 0){
+            addFirst(data);
             return;
         }
-        tail.next = head;
-        tail = newNode;
+        Node temp = head;
+        int i = 0;
+        while(i<index-1){
+            i++;
+            temp = temp.next;
+        }
+        Node newNode = new Node(data);
+        size++;
+        newNode.next = temp.next;
+        temp.next = newNode;
     }
 
     public static void main(String[] args) {
         LL l = new LL();
+        l.print();
         l.addFirst(2);
+        l.print();
         l.addFirst(1);
+        l.print();
         l.addLast(3);
+        l.print();
         l.addLast(4);
+        l.print();
+        System.out.println(l.size);
     }
 }
